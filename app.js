9749aac190f1e9434b58b24f8b7b38e95ca33823
Pozsgai Mate <pozsega1999@gmail.com>
2022-02-22 14:32:35 +0100

    rajzolas megvalositasa

--- app.js
+++ app.js
@@ -2,25 +2,46 @@ const container = document.getElementById('container');
 container.id = 'main';
 container.className = 'container';
 
-for(let i = 0; i<16;i++){
+
+const input = 16;
+const playAgain = document.getElementById('playAgain');
+playAgain.addEventListener('click', ()=>{
+        box.className= 'box';
+        input = prompt("Grid Size?");
+});
+
+
+
+function makeGrid(num){ 
+for(let i = 0; i<num;i++){
     let row = document.createElement('div');
     row.className = 'row';
     row.id = "row"+i;
     
-    for(let j = 0;j<16;j++){
-        let box = document.createElement('div');    
+    for(let j = 0;j<num;j++){
+        const box = document.createElement('div');    
         box.className = 'box'
         box.id = row.id +'box' + j; 
         row.appendChild(box);
 
-        box.addEventListener('mouseover',()=>{
-            box.className = 'box_hover';
+        box.addEventListener('mouseover',function(e){
+            if(e.buttons ==1){
+                e.target.className = 'box_hover';
+            }
         });
+        
+        
+
+
     }
         
     container.appendChild(row);
 }
 
+
+}
+
+makeGrid(30)
 
 
 
