67fa386e3a7538112bc2492921b256cd460cb1a7
Pozsgai Mate <pozsega1999@gmail.com>
2022-02-25 17:07:38 +0100

    final

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
+let color = 'black';
+let click = false;
 
 function makeBoard(size){
     if(size>100) alert("Max size is 100")
     let board = document.querySelector('.board');
     //le kell takaritani a kijelzot, mielott megvaltoztatjuk a tabla meretet
-    let squares = board.querySelectorAll('div');
-    squares.forEach((div)=>div.remove())
-
-    board.style.gridTemplateRows = `repeat(${size},1fr)`;
+        board.style.gridTemplateRows = `repeat(${size},1fr)`;
     board.style.gridTemplateColumns = `repeat(${size},1fr)`;
 
     
@@ -19,16 +18,40 @@ function makeBoard(size){
 }
 
 
-makeBoard(16);
+makeBoard(64);
 
 function changeBoardSize(input){
+    resetBoard();
     makeBoard(input);
 }
 
 function colorSquare(){
-    this.style.backgroundColor = 'black'; 
+    if(click){
+        if(color ==='random'){
+            this.style.backgroundColor = `hsl( ${Math.random() *360},100%, 50%)`
+        }else{
+            this.style.backgroundColor = color; 
+        }
+    }
 }
 
-function clearBoard(){
-    //TODO
+function changeColor(choice){
+    color = choice;
 }
+
+function resetBoard(){
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div)=>div.style.backgroundColor = 'white');
+}
+
+
+let board = document.querySelector('.board');
+board.addEventListener('mousedown', ()=>{
+        click = true;
+})
+
+board.addEventListener('mouseup', ()=>{
+    click  = false;
+})
+
